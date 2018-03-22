d579fb9c5dc0a950d0eb574bdc3e32a456cdf01d
mocamocaland <noi_kaas@yahoo.co.jp>
2018-03-22 20:27:38 +0900

    project/urls templatesをそれぞれ編集

--- project/project/urls.py
+++ project/project/urls.py
@@ -13,11 +13,41 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import render
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.decorators.csrf import ensure_csrf_cookie
+from django.contrib import admin
+from django.contirb.auth import views as auth_views
+
+from django.contrib.auth.decorators import login_required
+from rest_framework.documentation import include_docs_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('', include('noteapp.urls')),
-]
+    
+    # login,logout
+    path('login', auth_views.LoginView.as_view(template_name='login.html',
+                                               redirect_authenticated_user=true),
+                                               name='login'),
+    path('logout', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
+
+    # API ドキュメント
+    path('docs/', include_docs_urls(title='API', public=False)),
+
+    #  ノートアプリケーションAPI
+    path('note/', include('note.urls')),
+
+    # アプリケーションのUI
+    path('', ensure_csrf_cookie(login_required(render)),
+        kwargs={'tenplate_name': 'index.html'}, name='index'),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [
+        path(r'__debug__/', include('debug_toolbar.urls')),
+    ] + urlpatterns
 
